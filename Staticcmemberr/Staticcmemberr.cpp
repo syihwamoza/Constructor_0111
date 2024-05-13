d1fc299baee870f2598cdbfa90b362c4a31a795e
syihwamoza <mozakastella26@gmail.com>
2024-05-13 10:33:59 +0700

    memasukkan int mahasiswa dimulai dari 10

--- Staticcmemberr/Staticcmemberr.cpp
+++ Staticcmemberr/Staticcmemberr.cpp
@@ -5,4 +5,17 @@ using namespace std;
 class mahasiswa
 {
 public:
-}
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printA11();
+	mahasiswa (string pnama)
+	{
+		nama = pnama;
+		setID();
+	}
+};
+
+int mahasiswa::nim = 10;
