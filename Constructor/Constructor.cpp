b1a212115a3914f000886e557fd06c4baf4f4ed5
syihwamoza <mozakastella26@gmail.com>
2024-05-13 17:41:50 +0700

    mengakses nim melalui  static member function "getNim"

--- Constructor/Constructor.cpp
+++ Constructor/Constructor.cpp
@@ -2,11 +2,53 @@
 #include <string>
 using namespace std;
 
-class mahasiswa
+class Mahasiswa
 {
 public:
 	int nim;
 	string nama;
 
-public :
+public:
+	Mahasiswa()
+	{
+		nim = 0;
+		nama = " ";
+	};
+	Mahasiswa(int iNim)
+	{
+		nim = iNim;
+	}
+	Mahasiswa(string iNama)
+	{
+		nim = 0;
+		nama = iNama;
+	}
+	Mahasiswa(int iNim, string iNama)
+	{
+		nim = iNim;
+		nama = iNama;
+	};
+	void cetak()
+	{
+		cout << "NIM = " << nim << endl;
+		cout << "Nama = " << nama << endl;
+		cout << endl;
+	}
 };
+
+int main()
+{
+	Mahasiswa mhs1;
+	Mahasiswa mhs2(20);
+	Mahasiswa mhs3("Moza");
+	Mahasiswa mhs4(20, "Moza");
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+
+	return 0;
+
+}
+
